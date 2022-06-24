61e73189940150bccd5af79b555a66566a1ee6af
Michael Mohamed <michael.mohamed@echo1.io>
2022-06-24 11:02:20 -0400

    update

--- echo1_geopix/echo1_geopix.py
+++ echo1_geopix/echo1_geopix.py
@@ -18,83 +18,54 @@ class GeoPix:
 
     @beartype
     def get_geo_points_from_rel_pixel_points(
-        self, rel_x: Union[int, float], rel_y: Union[int, float]
+        self, min_x_rel: Union[int, float], min_y_rel: Union[int, float]
     ) -> Dict:
+
+        # min_lon, max_lon, max_lat, min_lat, min_x_rel, min_y_rel
+        # min_x_rel and min_y_rel pixel coords are relative to the max_lat min_lon
         return {
-            "lon": self.min_lon + (self.max_lon - self.min_lon) * (1 - rel_y),
-            "lat": self.min_lat + (self.max_lat - self.min_lat) * (rel_x),
+            "lat": self.min_lat + (self.max_lat - self.min_lat) * (1 - min_y_rel),
+            "lon": self.min_lon + (self.max_lon - self.min_lon) * (min_x_rel),
         }
 
-    @beartype
     def get_rel_pixel_points_from_geo_points(
         self, lat: Union[int, float], lon: Union[int, float]
     ) -> Dict:
+        # min_lon, max_lon, max_lat, min_lat, lon, lat
+        # x and y geo coords are in latitude, longitude
         return {
-            "x": ((lat - self.min_lat) / (self.max_lat - self.min_lat)),
-            "y": ((self.max_lon - lon) / (self.max_lon - self.min_lon)),
+            "x": (lon - self.min_lon) / (self.max_lon - self.min_lon),
+            "y": (self.max_lat - lat) / (self.max_lat - self.min_lat),
         }
 
-    @beartype
     def get_geo_box_from_rel_pixel_box(
         self,
         min_x_rel: Union[int, float],
-        min_y_rel: Union[int, float],
         max_x_rel: Union[int, float],
+        min_y_rel: Union[int, float],
         max_y_rel: Union[int, float],
     ) -> Dict:
+
+        # return coords for bounding box in lon,lat format
         return {
-            "min_lon": self.max_lon - (self.max_lon - self.min_lon) * min_y_rel,
-            "max_lon": self.max_lon - (self.max_lon - self.min_lon) * max_y_rel,
-            "min_lat": self.min_lat + (self.max_lat - self.min_lat) * min_x_rel,
-            "max_lat": self.min_lat + (self.max_lat - self.min_lat) * max_x_rel,
+            "min_lat": self.max_lat - (self.max_lat - self.min_lat) * min_y_rel,
+            "max_lat": self.max_lat - (self.max_lat - self.min_lat) * max_y_rel,
+            "min_lon": self.min_lon + (self.max_lon - self.min_lon) * min_x_rel,
+            "max_lon": self.min_lon + (self.max_lon - self.min_lon) * max_x_rel,
         }
 
-    @beartype
     def get_rel_pixel_box_from_geo_box(
         self,
-        _min_lat: Union[int, float],
-        _max_lat: Union[int, float],
-        _min_lon: Union[int, float],
-        _max_lon: Union[int, float],
+        min_lat: Union[int, float],
+        max_lat: Union[int, float],
+        min_lon: Union[int, float],
+        max_lon: Union[int, float],
     ) -> Dict:
-
-        # lon_min = _min_lon
-        # lat_min = _min_lat
-        # lon_max = _max_lon
-        # lat_max = _max_lat
-
-        # top = self.max_lat
-        # bottom = self.min_lat
-        # left = self.min_lon
-        # right = self.max_lon
-
-        lon_min = _min_lon
-        lat_min = _min_lat
-        lon_max = _max_lon
-        lat_max = _max_lat
-
-        top = self.max_lat
-        bottom = self.min_lat
-        left = self.min_lon
-        right = self.max_lon
-
-        # print(100)
-        # print(left, self.min_lon, lon_min, _min_lon)
-        # print(bottom, self.min_lat, lat_min, _min_lat)
-        # print(right, self.max_lon, lon_max, _max_lon)
-        # print(top, self.max_lat, lat_max, _max_lat)
-
         # return coords for bounding box in pixel coords for a specific image
         # as percentage of image width and height
-        x_min = (lon_min - left) / (right - left)
-        x_max = (lon_max - left) / (right - left)
-        y_min = (top - lat_min) / (top - bottom)
-        y_max = (top - lat_max) / (top - bottom)
-        return {"x_min": x_min, "y_min": y_min, "x_max": y_min, "y_max": y_max}
-
-        # return {
-        #     "min_x": (self.min_lon - _min_lon) / (_max_lat - _min_lat),
-        #     "max_x": (_max_lat - self.min_lat) / (_max_lon - _min_lon),
-        #     "min_y": (_max_lat - self.min_lat) / (_max_lon - _min_lon),
-        #     "max_y": (_max_lat - self.max_lat) / (_max_lon - _min_lon),
-        # }
+        return {
+            "min_x_rel": (min_lon - self.min_lon) / (self.max_lon - self.min_lon),
+            "max_x_rel": (self.max_lat - min_lat) / (self.max_lat - self.min_lat),
+            "min_y_rel": (self.max_lat - min_lat) / (self.max_lat - self.min_lat),
+            "max_y_rel": (self.max_lat - max_lat) / (self.max_lat - self.min_lat),
+        }
--- tests/test_echo1_geopix.py
+++ tests/test_echo1_geopix.py
@@ -2,13 +2,12 @@ from echo1_geopix.echo1_geopix import GeoPix
 from loguru import logger
 from math import isclose
 
-# test values (rel pixel values)
+# test values
 min_x_rel = 0.35596034
-min_y_rel = 0.94408214
 max_x_rel = 0.4102673
-max_y_rel = 0.9986186
 
-# test values (geo values)
+min_y_rel = 0.94408214
+max_y_rel = 0.9986186
 
 min_lat = 19.003535899073533
 max_lat = 19.013473367825767
@@ -20,63 +19,66 @@ max_lon = -98.26036944570143
 def test_point_funcs():
 
     ##
-    # pixel_point_2_geo_point
+    # get_geo_points_from_rel_pixel_points
     ##
-    gp = GeoPix(min_lat, max_lat, min_lon, max_lon)
+    gp = GeoPix(
+        min_lat,
+        max_lat,
+        min_lon,
+        max_lon,
+    )
     tmp_geo_coords = gp.get_geo_points_from_rel_pixel_points(min_x_rel, min_y_rel)
-
-    # return
-    assert tmp_geo_coords == {"lon": -98.27023260911693, "lat": 19.007073243829318}
-    # logger.debug("pixel_point_2_geo_point: {}".format(tmp_geo_coords))
+    assert tmp_geo_coords == {"lon": -98.26709795813007, "lat": 19.004091581059974}
 
     ##
-    # geo_point_2_pix_point
+    # get_rel_pixel_points_from_geo_points
     ##
-    gp = GeoPix(min_lat, max_lat, min_lon, max_lon)
+    gp = GeoPix(
+        min_lat,
+        max_lat,
+        min_lon,
+        max_lon,
+    )
+
     tmp_pixel_coords = gp.get_rel_pixel_points_from_geo_points(
         tmp_geo_coords["lat"], tmp_geo_coords["lon"]
     )
-    assert tmp_pixel_coords == {"x": 0.3559603399999817, "y": 0.9440821400005416}
+
     assert isclose(min_x_rel, tmp_pixel_coords["x"], abs_tol=1e-8)
     assert isclose(min_y_rel, tmp_pixel_coords["y"], abs_tol=1e-8)
-    # logger.debug("geo_point_2_pix_point: {}".format(tmp_pixel_coords))
+    assert tmp_pixel_coords == {"x": 0.3559603399996717, "y": 0.9440821400000854}
 
 
 def test_box_funcs():
-
     ##
-    # pixel_box_2_geo_box
+    # get_geo_box_from_rel_pixel_box
     ##
-    gp = GeoPix(min_lat, max_lat, min_lon, max_lon)
+    gp = GeoPix(
+        min_lat,
+        max_lat,
+        min_lon,
+        max_lon,
+    )
     tmp_geo_box = gp.get_geo_box_from_rel_pixel_box(
-        min_x_rel,
-        min_y_rel,
-        max_x_rel,
-        max_y_rel,
+        min_x_rel, max_x_rel, min_y_rel, max_y_rel
     )
 
     assert tmp_geo_box == {
-        "min_lon": -98.27023260911693,
-        "max_lon": -98.27080237099918,
-        "min_lat": 19.007073243829318,
-        "max_lat": 19.00761291754735,
+        "min_lon": -98.26709795813007,
+        "min_lat": 19.004091581059974,
+        "max_lon": -98.26653059391631,
+        "max_lat": 19.003549626692866,
     }
 
-    # logger.debug("pixel_box_2_geo_box: {}".format(tmp_geo_box))
-
     ##
-    # geo_box_2_pixel_box
+    # get_rel_pixel_box_from_geo_box
     ##
-    print(200, min_lat)
-    gp = GeoPix(min_lat, max_lat, min_lon, max_lon)
-
-    # print(100)
-    # print(
-    #     tmp_geo_box["min_lat"],
-    #     tmp_geo_box["max_lat"],
-    #     tmp_geo_box["min_lon"],
-    #     tmp_geo_box["max_lon"],
-    # )
+    gp = GeoPix(
+        min_lat,
+        max_lat,
+        min_lon,
+        max_lon,
+    )
     temp_pixel_box = gp.get_rel_pixel_box_from_geo_box(
         tmp_geo_box["min_lat"],
         tmp_geo_box["max_lat"],
@@ -84,12 +86,9 @@ def test_box_funcs():
         tmp_geo_box["max_lon"],
     )
 
-    print(temp_pixel_box)
-    # assert temp_pixel_box == {
-    #     "min_x": 0.3559603399996717,
-    #     "min_y": 0.9440821400000854,
-    #     "max_x": 0.9440821400000854,
-    #     "max_y": 0.998618600000152,
-    # }
-
-    # # logger.debug("geo_box_2_pixel_box: {}".format(temp_pixel_box))
+    assert temp_pixel_box == {
+        "min_x_rel": 0.3559603399996717,
+        "max_x_rel": 0.9440821400000854,
+        "min_y_rel": 0.9440821400000854,
+        "max_y_rel": 0.998618600000152,
+    }
